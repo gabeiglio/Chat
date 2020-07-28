e3679259bacbaee90c89dd0af8086dcc557a512b
Gabriel Igliozzi <gaboiglio@gmail.com>
2020-07-28 16:44:42 -0400

    initial commit

---
+++ Chat/Controller/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Gabriel Igliozzi on 7/28/20.
+//  Copyright © 2020 Gabriel Igliozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+    
+}
