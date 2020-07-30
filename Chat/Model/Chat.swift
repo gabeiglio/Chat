7c7655cd9dfa8a7aa1429e4788875978bf8386a3
Gabriel Igliozzi <gaboiglio@gmail.com>
2020-07-29 20:28:36 -0400

    added gitignore, ignore Pods

---
+++ Chat/Model/Chat.swift
@@ -0,0 +1,9 @@
+//
+//  Chat.swift
+//  Chat
+//
+//  Created by Gabriel Igliozzi on 7/29/20.
+//  Copyright © 2020 Gabriel Igliozzi. All rights reserved.
+//
+
+import Foundation
---
+++ Chat/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Chat
+//
+//  Created by Gabriel Igliozzi on 7/29/20.
+//  Copyright © 2020 Gabriel Igliozzi. All rights reserved.
+//
+
+import Foundation
