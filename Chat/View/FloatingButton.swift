36cdfbb6f719402eacc26013311678c976f35e0d
Gabriel Igliozzi <gaboiglio@gmail.com>
2020-08-01 23:48:49 -0400

    Created custom floating button to create new chat

--- Chat/View/FloatingButton.swift
+++ Chat/View/FloatingButton.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2020 Gabriel Igliozzi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FloatingButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //Self
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.tintColor = .white
+        self.setImage(UIImage(systemName: "pencil"), for: .normal)
+        self.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        self.backgroundColor = UIColor(red: 255/255, green: 87/255, blue: 34/255, alpha: 1)
+        
+        //Shadow
+        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+        self.layer.masksToBounds = false
+        self.layer.cornerRadius = 4.0
+        
+        self.layer.cornerRadius = 60 / 2
+
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+}
