fbfc9306adaf89d24c98e18c59caf72457852ca5
Gabriel Igliozzi <gaboiglio@gmail.com>
2020-08-02 23:23:57 -0400

    Added collection view and initial state of diffable datasource

--- Chat/Controller/ChatViewController.swift
+++ Chat/Controller/ChatViewController.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class ChatViewController: UIViewController {
     
+    private let collectionView: UICollectionView = {
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        
+        let collection = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.backgroundColor = .white
+        collection.register(MessagesCollectionViewCell.self, forCellWithReuseIdentifier: "messageCell")
+        return collection
+    }()
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Message>!
+    
     private let inputTextView = InputTextView()
+    
+    public var sender: User!
+    public var receiver: User!
+    
+    private var messages = [Message]()
     
     //Constraints
     private var inputTextBottomConstraint: NSLayoutConstraint?
     private var inputTextHeightConstraint: NSLayoutConstraint?
     
+    //Enun for sections
+    private enum Section {
+        case main
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         //Add notifications for keyboard events (hide/show)
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -25,6 +51,7 @@ class ChatViewController: UIViewController {
         
         //Delegate
         self.inputTextView.delegate = self
+        self.collectionView.delegate = self
         
         //Setup self
         self.view.backgroundColor = .white
@@ -42,21 +69,69 @@ class ChatViewController: UIViewController {
             self.inputTextView.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor),
             self.inputTextView.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor),
         ])
+        
+        //Setup collection view
+        self.view.addSubview(self.collectionView)
+        NSLayoutConstraint.activate([
+            self.collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.collectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            self.collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            self.collectionView.bottomAnchor.constraint(equalTo: self.inputTextView.topAnchor)
+        ])
+        
+        self.configureDataSource()
+        self.createSnapshot(from: [Message(id: "1", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "2", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "3", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "4", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "5", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "6", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "7", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "8", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "9", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "10", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "11", sender: self.sender, receiver: self.receiver, payload: "Hi how are you"),Message(id: "12", sender: self.sender, receiver: self.receiver, payload: "Hi how are you")])
+        
+        Network.observeMessages { (result) in
+            
+        }
     }
+    
+}
+
+//Handle collectionview delegate and methods
+extension ChatViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    private func configureDataSource() {
+        let dataSource = UICollectionViewDiffableDataSource<Section, Message>(collectionView: self.collectionView) { (collectionView, indexPath, message) -> UICollectionViewCell? in
+            
+            guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "messageCell", for: indexPath) as? MessagesCollectionViewCell else { return UICollectionViewCell() }
+            cell.setupCell(message: message.payload)
+            return cell
+            
+        }
+        self.dataSource = dataSource
+    }
+    
+    private func createSnapshot(from messages: [Message]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Message>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(messages)
+        self.dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    internal func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.collectionView.frame.size.width / 1.8, height: 50)
+    }
+    
 }
 
 //Handle input text view delegate
 extension ChatViewController: InputTextViewDelegate {
     internal func didTapSendButton(text: String) {
-        
-        self.inputTextHeightConstraint?.constant = 0
+        Network.sendMessage(message: Message(id: UUID().uuidString, sender: self.sender, receiver: self.receiver, payload: text))
+        self.inputTextHeightConstraint?.constant = 50
         self.view.layoutIfNeeded()
+    }
+    
+    internal func didTapMoreButton() {
+        
     }
     
     internal func textViewDidChangeHeight(height: CGFloat) {
         self.inputTextHeightConstraint?.constant += height
         self.view.layoutIfNeeded()
     }
+    
 }
 
 //Handle Keyboard
