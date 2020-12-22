1ef29e1a53c30fd40bb656716f255efd0a674435
Gabriel Igliozzi <gaboiglio@gmail.com>
2020-12-22 18:17:39 -0500

    Progress on messages cell

--- Chat/View/Messages Cell/MessagesCollectionViewCell.swift
+++ Chat/View/Messages Cell/MessagesCollectionViewCell.swift
@@ -16,22 +16,46 @@ class MessagesCollectionViewCell: UICollectionViewCell {
         textview.tintColor = .white
         textview.isEditable = false
         textview.translatesAutoresizingMaskIntoConstraints = false
+        textview.font = UIFont.systemFont(ofSize: 15)
         return textview
     }()
+    
+    private let container: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    //Constraints
+    var rightConstraint: NSLayoutConstraint?
+    var leftConstraint: NSLayoutConstraint?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.layer.cornerRadius = 10
-        self.backgroundColor = UIColor(red: 255/255, green: 87/255, blue: 34/255, alpha: 1)
+        //Setup view
+        self.addSubview(self.container)
         
-        //Setup textview
-        self.addSubview(self.textView)
+        //Setup container
+        self.container.layer.cornerRadius = 5
+        self.rightConstraint = self.container.rightAnchor.constraint(equalTo: self.rightAnchor)
+        self.rightConstraint?.isActive = true
+        
+        self.leftConstraint = self.container.leftAnchor.constraint(equalTo: self.leftAnchor)
+        self.leftConstraint?.isActive = true
+        
         NSLayoutConstraint.activate([
-            self.textView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 5),
-            self.textView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -5),
-            self.textView.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
-            self.textView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5)
+            self.container.topAnchor.constraint(equalTo: self.topAnchor),
+            self.container.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+                
+        //Setup textview
+        self.container.addSubview(self.textView)
+        NSLayoutConstraint.activate([
+            self.textView.leftAnchor.constraint(equalTo: self.container.leftAnchor, constant: 5),
+            self.textView.rightAnchor.constraint(equalTo: self.container.rightAnchor, constant: -5),
+            self.textView.topAnchor.constraint(equalTo: self.container.topAnchor, constant: 5),
+            self.textView.bottomAnchor.constraint(equalTo: self.container.bottomAnchor, constant: -5)
         ])
     }
     
@@ -39,7 +63,19 @@ class MessagesCollectionViewCell: UICollectionViewCell {
         super.init(coder: coder)
     }
     
-    public func setupCell(message: String) {
+    public func setupCell(message: String, isSender: Bool) {
         self.textView.text = message
+        
+        if isSender {
+            self.container.backgroundColor = UIColor(red: 255/255, green: 87/255, blue: 34/255, alpha: 1)
+            self.textView.textAlignment = .right
+            self.leftConstraint?.constant += self.frame.size.width / 3
+            self.textView.textColor = .black
+        } else {
+            self.container.backgroundColor = .blue
+            self.textView.textAlignment = .left
+            self.rightConstraint?.constant -= self.frame.size.width / 3
+            self.textView.textColor = .white
+        }
     }
 }
